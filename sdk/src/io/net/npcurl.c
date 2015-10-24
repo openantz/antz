9fc2fb6feb37a09f8702b0624d5e804d5f2a305b
saxondigital <makecontact@saxondigital.net>
2015-10-23 18:32:31 -0700

    added low-med resolution app icon

--- sdk/src/io/net/npcurl.c
+++ sdk/src/io/net/npcurl.c
@@ -1,10 +1,27 @@
-	//
-//  npcurl.c
-//  npcurl
-//
-//  Created by z on 6/20/15.
-//  Copyright (c) 2015 z. All rights reserved.
-//
+/* -----------------------------------------------------------------------------
+*
+*  npcurl.c
+*
+*  ANTz - realtime 3D data visualization tools for the real-world, based on NPE.
+*
+*  ANTz is hosted at http://openantz.com and NPE at http://neuralphysics.org
+*
+*  Written in 2010-2015 by Shane Saxon - saxon@openantz.com
+*
+*  Please see main.c for a complete list of additional code contributors.
+*
+*  To the extent possible under law, the author(s) have dedicated all copyright 
+*  and related and neighboring rights to this software to the public domain
+*  worldwide. This software is distributed without any warranty.
+*
+*  Released under the CC0 license, which is GPL compatible.
+*
+*  You should have received a copy of the CC0 Public Domain Dedication along
+*  with this software (license file named LICENSE.txt). If not, see
+*  http://creativecommons.org/publicdomain/zero/1.0/
+*
+* --------------------------------------------------------------------------- */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
